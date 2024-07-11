c223d6f3827f1ac7d29fbf9fca19c3dfbbfe6cea
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-11 15:56:01 +0700

    feat:comlete ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+
+const numbers = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(numbers, target));
